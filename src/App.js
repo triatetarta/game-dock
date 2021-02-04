37996092452927eb5976a614c7bd0f7ee4f8dc96
Dimitrios Chatziioannou <dimchatziioannou@gmail.com>
2021-02-04 10:16:21 +0000

    axios fetch data done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Home from './pages/Home';
 
 const App = () => {
   return (
     <div className='App'>
-      <h1>Game Dock</h1>
+      <Home />
     </div>
   );
 };
---
+++ src/reducers/gamesReducer.js
@@ -0,0 +1,23 @@
+const initState = {
+  popular: [],
+  newGames: [],
+  upcoming: [],
+  searched: [],
+};
+
+const gamesReducer = (state = initState, action) => {
+  switch (action.type) {
+    case 'FETCH_GAMES':
+      return {
+        ...state,
+        popular: action.payload.popular,
+        newGames: action.payload.newGames,
+        upcoming: action.payload.upcoming,
+      };
+
+    default:
+      return { ...state };
+  }
+};
+
+export default gamesReducer;
